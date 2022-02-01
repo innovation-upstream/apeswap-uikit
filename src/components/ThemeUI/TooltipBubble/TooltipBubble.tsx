25a94c49f53eb0dc56861d532ef98f59c0443180
Isioma <isiomaogbuehi@gmail.com>
2022-02-01 16:08:02 +0100

    Added more direction variants

--- src/components/ThemeUI/TooltipBubble/TooltipBubble.tsx
+++ src/components/ThemeUI/TooltipBubble/TooltipBubble.tsx
@@ -3,7 +3,12 @@ import { Box } from "theme-ui";
 import { TooltipProps } from "./TooltipBubble.interface";
 import style from "./styles";
 
-const TooltipBubble: React.FC<TooltipProps> = ({ variant = "4", maxWidth = "250px", direction="bottomRight", children }) => {
+const TooltipBubble: React.FC<TooltipProps> = ({
+  variant = "4",
+  maxWidth = "250px",
+  direction = "bottomRight",
+  children,
+}) => {
   return <Box sx={{ maxWidth, ...style[variant], ...style[`${direction}`] }}>{children}</Box>;
 };
 
