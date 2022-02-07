bf3c0c9bdf3a4ef10d12e375f2622aa6e8ccd7e9
Isioma <isiomaogbuehi@gmail.com>
2022-02-07 19:12:22 +0100

    rebased from milestone-2

--- src/__tests__/components/tooltip.test.tsx
+++ src/__tests__/components/tooltip.test.tsx
@@ -6,22 +6,16 @@ it("renders correctly", () => {
   const { asFragment } = renderWithTheme(<TooltipBubble body={<></>}>Tooltip</TooltipBubble>);
   expect(asFragment()).toMatchInlineSnapshot(`
     <DocumentFragment>
-      <div
-        class="css-1s6czo2"
-      >
-        <div
-          class="css-140ll04"
-        >
-          <div
-            class="css-1d82qvs"
-          />
-        </div>
-        <div
-          class="css-r8xm40"
-        >
-          Tooltip
-        </div>
-      </div>
+        <span class="css-1aic9ui-TooltipBubble css-vurnku">
+            <div class="css-1t2ffmq-TooltipBubble css-vurnku">
+                <div class="css-4ktmrq-TooltipBubble css-vurnku">
+                    <span class="css-lo32p-Text css-vurnku">Just Text. Variant 4</span>
+                </div>
+            </div>
+            <span class="css-vurnku">
+                <button class="css-1m4z738-Button css-vhv7r1">Just Text. Variant 4</button>
+            </span>
+        </span>
     </DocumentFragment>
   `);
 });
